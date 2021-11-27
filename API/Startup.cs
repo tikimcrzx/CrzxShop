dc780dd3b7edb51df6e28bd732d24029ba4ff219
tikimcrzx <tikimioo723@gmail.com>
2021-11-26 19:30:27 -0800

    Add the GenereicPatternRepo and Afther add Specification Pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
