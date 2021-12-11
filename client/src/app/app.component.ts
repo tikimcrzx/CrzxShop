88a0d8425cff7dde5024d162fcae6bfc93ba77f8
tikimcrzx <tikimioo723@gmail.com>
2021-12-10 20:04:57 -0800

    Add Angular client configure HttpModule

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { IPagination } from './models/pagination';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'CrzxShop';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
